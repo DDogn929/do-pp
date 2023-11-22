2634fcc86446885d26d322bea649eb9d8077446c
DDogn929 <130423139+DDogn929@users.noreply.github.com>
2023-11-22 15:05:57 +0900

    11/22

---
+++ M/JS/mc.js
@@ -0,0 +1,16 @@
+const optionSelectList = document.getElementsByClassName("option-select-list")
+
+
+const leftButton = optionSelectList[0].querySelector('.option-left-button')
+const rightButton = optionSelectList[0].querySelector('.option-right-button')
+const slider = optionSelectList[0].querySelector('ul')
+
+.addEventListener("click",)
+
+
+
+// const ul = document.getElementsByClassName("option-slide-container")
+// const optionList = document.getElementsByClassName("option-select-list-button")
+// const leftButton = document.getElementsByClassName("option-left-button")
+// const rightButton = document.getElementsByClassName("option-right-button")
+
