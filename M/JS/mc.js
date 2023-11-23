45262b7109e4b315d27b6e8e9e1b30231c097c3e
DDogn929 <130423139+DDogn929@users.noreply.github.com>
2023-11-24 00:30:36 +0900

    11/24

--- M/JS/mc.js
+++ M/JS/mc.js
@@ -1,13 +1,42 @@
-const optionSelectList = document.getElementsByClassName("option-select-list")
-
+const optionSelectList = document.getElementsByClassName('option-select-list')
 
 const leftButton = optionSelectList[0].querySelector('.option-left-button')
 const rightButton = optionSelectList[0].querySelector('.option-right-button')
 const slider = optionSelectList[0].querySelector('ul')
+const optionSelectButton =  optionSelectList[0].querySelector('.option-select-list-button')
 
-.addEventListener("click",)
+let index = 0;
+
+console.log()
+
+leftButton.addEventListener('click',()=>{
+
+    index--
+    
+    if(index >= 0) {
+        slider.style.transform = `translateX(calc((-132px + -16px)*${index}))`
+    }
+    
+    // alert('이것은 왼쪽버튼입니다');
+})
+
+rightButton.addEventListener('click',()=>{
+
+    index++
+
+    if(index < slider.childElementCount) {
+        slider.style.transform = `translateX(calc((-132px + -16px)*${index}))`
+    }
+
+    // alert('이것은 오른쪽버튼입니다');
+})
+
+// for (let i = 0; i < optionSelectButton.length; i++) {
+    
+// }
 
 
+// transform: translateX(calc((-132px + -16px)*index));
 
 // const ul = document.getElementsByClassName("option-slide-container")
 // const optionList = document.getElementsByClassName("option-select-list-button")
